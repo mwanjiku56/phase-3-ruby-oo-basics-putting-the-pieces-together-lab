d66c0d5bf3fdc8819f884737ac51df8fb1317054
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-19 22:21:02 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_accessor:title
+    attr_accessor:author
+    attr_accessor:page_count
+    attr_accessor:genre
+    def initialize (title)
+        @title=title
+        @author=author
+        @page_count=page_count
+        @genre=genre
+    end
+
+    def turn_page
+        puts"Flipping the page...wow, you read fast!"
+    end
+end
+book=Book.new("And Then There Were None")
+# book.title ="And Then There Were None"
+
+# book.author= "Agatha Christie"
+# book.page_count = 272
+# book.genre = "Mystery"
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor:brand,:color,:size,:material,:condition
+    def initialize(brand)
+        @brand=brand
+        @color=color
+
+    end
+    def cobble
+		@condition = "new"
+		puts("Your shoe is as good as new!")
+
+	end
+end
+shoe=Shoe.new("Adidas")
+
+# shoe.color= "red"
+# shoe.brand="Nike"
